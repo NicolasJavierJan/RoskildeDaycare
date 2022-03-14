e0d47678b4d4166250b7995e96b0caa374838ba3
andreaDiClaudio <andreadicla@gmail.com>
2022-03-14 11:18:59 +0100

    Created database "daycare", created ddl.sql and dml.sql files. Inserted fixed values in the database

---
+++ src/Sql/ddl.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE IF NOT EXISTS daycare;
+
+USE daycare;
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username varchar(100) NOT NULL,
+    password varchar(30) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS employees(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    morning BOOLEAN NOT NULL,
+    afternoon BOOLEAN NOT NULL,
+    phone_number INT NOT NULL,
+    e_mail VARCHAR(100) NOT NULL,
+    FOREIGN KEY (id) REFERENCES users(id)
+    );
+
+CREATE TABLE IF NOT EXISTS parents(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    phone_number INT NOT NULL,
+    e_mail VARCHAR(100) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS children(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL,
+    extra_info VARCHAR(500) NOT NULL,
+    parent_id INT NOT NULL,
+    FOREIGN KEY (parent_id) REFERENCES parents(id)
+    );
+
+CREATE TABLE IF NOT EXISTS children_list(
+     child_id INT NOT NULL,
+     FOREIGN KEY (child_id) REFERENCES children(id)
+    );
---
+++ src/Sql/dml.sql
@@ -0,0 +1,23 @@
+-- INSERT INTO daycare.users (username, password) VALUES ("andrea", "Andrea1");
+-- INSERT INTO daycare.users (username, password) VALUES ("nicolas", "Nico2");
+
+
+-- INSERT INTO daycare.employees (first_name, last_name, morning, afternoon, phone_number, e_mail) VALUES ("Andrea", "Di Claudio", true, false, 28769466, "andreadicla@gmail.com");
+-- INSERT INTO daycare.employees (first_name, last_name, morning, afternoon, phone_number, e_mail) VALUES ("Nicolas", "Javier Jan", false, true, 71368801, "nicolas.javier.jan@gmail.com");
+
+-- INSERT INTO daycare.parents (first_name, last_name, phone_number, e_mail) VALUES ("Jasmin", "Aloise", 33333333, "jasminaloise@gmail.com");
+-- INSERT INTO daycare.parents (first_name, last_name, phone_number, e_mail) VALUES ("Flor", "Batipalla", 55555555, "florbatipalla@gmail.com");
+
+-- INSERT INTO daycare.children(first_name, last_name, birth_date, extra_info, parent_id) VALUES ("Douglas", "Teacher", "2018-01-01", " ", 1);
+-- INSERT INTO daycare.children(first_name, last_name, birth_date, extra_info, parent_id) VALUES ("Emil", "Student", "2017-02-02", " ", 2);
+-- INSERT INTO daycare.children(first_name, last_name, birth_date, extra_info, parent_id) VALUES ("Martin", "Student", "2017-12-17", "no cigarettes ", 2);
+-- INSERT INTO daycare.children(first_name, last_name, birth_date, extra_info, parent_id) VALUES ("Nayeem", "Student", "2018-07-25", " ", 2);
+-- INSERT INTO daycare.children(first_name, last_name, birth_date, extra_info, parent_id) VALUES ("Christian", "Teacher", "2020-05-09", "no strawberries ", 1);
+-- INSERT INTO daycare.children(first_name, last_name, birth_date, extra_info, parent_id) VALUES ("Nika", "Student", "2019-04-21", " Allergic to peanuts ", 2);
+
+-- INSERT INTO daycare.children_list(child_id) VALUES (1);
+-- INSERT INTO daycare.children_list(child_id) VALUES (2);
+-- INSERT INTO daycare.children_list(child_id) VALUES (3);
+-- INSERT INTO daycare.children_list(child_id) VALUES (4);
+-- INSERT INTO daycare.children_list(child_id) VALUES (5);
+-- INSERT INTO daycare.children_list(child_id) VALUES (6);
